271e66a301e5515aa59bf4bf14693f72ec00de60
Abdurrahman Shofy Adianto <azophy@gmail.com>
2024-11-29 21:23:40 +0700

    chore: linting

--- lib/crypto.test.ts
+++ lib/crypto.test.ts
@@ -3,19 +3,18 @@ import { generateKeyPair, encryptAsym, decryptAsym } from './crypto'
 
 test('basic crypto operation', () => {
 	const plaintext = 'cobacoba'
-const keys = generateKeyPair()
-expect(keys).toHaveProperty('publicKey')
-expect(keys).toHaveProperty('privateKey')
-expect(keys).not.toHaveProperty('err')
-const secret = encryptAsym(plaintext, keys.publicKey)
-expect(secret).not.toEqual(plaintext)
-expect(decryptAsym(secret, keys.privateKey)).toEqual(plaintext)
-
-  })
+  const keys = generateKeyPair()
+  expect(keys).toHaveProperty('publicKey')
+  expect(keys).toHaveProperty('privateKey')
+  expect(keys).not.toHaveProperty('err')
+  const secret = encryptAsym(plaintext, keys.publicKey)
+  expect(secret).not.toEqual(plaintext)
+  expect(decryptAsym(secret, keys.privateKey)).toEqual(plaintext)
+})
 
 test('splitting privateKey with SSS', () =>{
-const keys = generateKeyPair()
-expect(keys).not.toHaveProperty('err')
-expect(keys).toHaveProperty('publicKey')
-expect(keys).toHaveProperty('privateKey')
+  const keys = generateKeyPair()
+  expect(keys).not.toHaveProperty('err')
+  expect(keys).toHaveProperty('publicKey')
+  expect(keys).toHaveProperty('privateKey')
 })
--- lib/crypto.ts
+++ lib/crypto.ts
@@ -2,32 +2,34 @@
 import * as crypto from 'crypto'
 
 export function generateKeyPair() {
-const res = crypto.generateKeyPairSync('rsa', {
-modulusLength: 4096,
-publicKeyEncoding: {
-type: 'spki',
-format: 'pem',
-},
-privateKeyEncoding: {
-type: 'pkcs8',
-format: 'pem',
-//cipher: 'aes-256-cbc',
-//passphrase: 'top secret',
-},
-}) 
-return res
+  const res = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+      type: 'spki',
+      format: 'pem',
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'pem',
+      //cipher: 'aes-256-cbc',
+      //passphrase: 'top secret',
+    },
+  })
+  return res
 }
 
 // ref: https://dev.to/superviz/implementing-symmetric-and-asymmetric-encryption-with-nodejs-4efp
 export const encryptAsym = (text, publicKey) => {
-	  return crypto.publicEncrypt(publicKey, Buffer.from(text, 'utf8')).toString('base64');
+  return crypto.publicEncrypt(publicKey, Buffer.from(text, 'utf8')).toString('base64');
 }
 
 export const decryptAsym = (encryptedText, privateKey) => {
-	return crypto.privateDecrypt({
-	key: privateKey,
-	// passphrase
-	}, Buffer.from(encryptedText, 'base64')).toString('utf8');
+	return crypto.privateDecrypt(
+    {
+      key: privateKey,
+      // passphrase
+    }, Buffer.from(encryptedText, 'base64')
+  ).toString('utf8');
 }
 
 
