51ec43cde77a9c5112d0bfca66dec0b529a4b8ca
Abdurrahman Shofy Adianto <azophy@gmail.com>
2024-11-29 14:18:52 +0000

    add shamir-secret-sharing package

--- lib/crypto.test.ts
+++ lib/crypto.test.ts
@@ -12,3 +12,10 @@ expect(secret).not.toEqual(plaintext)
 expect(decryptAsym(secret, keys.privateKey)).toEqual(plaintext)
 
   })
+
+test('splitting privateKey with SSS', () =>{
+const keys = generateKeyPair()
+expect(keys).not.toHaveProperty('err')
+expect(keys).toHaveProperty('publicKey')
+expect(keys).toHaveProperty('privateKey')
+})
